729ad78ead3f257dd74dd4a6c054931d556e7021
luobin <1096422441@qq.com>
2020-04-09 09:13:59 +0800

    ‘搭建初始环境’

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from 'antd/es/button'
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
+
+
+export default App;
