72175402ee2a0607632b7eb97e0712d56563e68b
luobin <1096422441@qq.com>
2020-05-10 09:35:53 +0800

    footer添加联系方式github和juejin

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import './Footer.scss';
+import {GithubOutlined} from '@ant-design/icons/lib';
 
-class Footer extends React.Component{
+class Footer extends React.Component {
   render() {
     return (
       <footer>
         <div className='content'>
-          <span>© 番茄时间</span>
-          <span>联系作者</span>
+         <div>
+           <span>© 番茄时间</span>
+         </div>
+          <div>
+            <span>联系作者</span>
+            <a href="https://github.com/Luobinf" target="_blank">
+              <GithubOutlined className="github"/>
+            </a>
+            <a href="https://juejin.im/user/5cf0beea5188255818782013/posts" target="_blank">
+              <svg className="icon juejin" aria-hidden="true">
+                <use xlinkHref="#icon-juejin"/>
+              </svg>
+            </a>
+          </div>
         </div>
       </footer>
     );
