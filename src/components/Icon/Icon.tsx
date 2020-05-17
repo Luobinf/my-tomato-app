5f68b3f5b7de6f09d48609765c3057de5c7a469c
luobin <1096422441@qq.com>
2020-05-17 15:50:19 +0800

    使用svg sprite loader，并将Icon封装成一个组件

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import tomato from '../../icons/tomato.svg';
-console.log(tomato);
+// import tomato from '../../icons/tomato.svg';
+// console.log(tomato);
 
 interface Props {
   name: string
+}
+
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('../../icons', true, /\.svg$/));
+} catch (error) {
+  console.log(error);
 }
 
 const Icon = (props: Props) => {
